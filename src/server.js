3aaf6e88f4f7037436a39f5c80ad9485994955ab
Adnan <adnan_ghafoor94@hotmail.com>
2020-08-01 17:14:39 +0100

    Starting html route

--- src/server.js
+++ src/server.js
@@ -5,3 +5,12 @@ const htmlRoutes = require("./routes/html-routes");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
