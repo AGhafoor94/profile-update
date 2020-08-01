2a7b2c24e9eb89b29a1aa8c5c61eb3f0157e2106
Adnan <adnan_ghafoor94@hotmail.com>
2020-08-01 20:11:10 +0100

    Working on portfolio website

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,5 +1,8 @@
 const home = document.getElementById("home");
 const portfolio = document.getElementById("portfolio");
+const burger = document.getElementById("burger");
+const navBar = document.getElementById("navbarMenuHeroA");
+
 if (window.location.pathname === "/portfolio") {
   home.classList.remove("is-active");
   portfolio.classList.add("is-active");
@@ -7,3 +10,12 @@ if (window.location.pathname === "/portfolio") {
   home.classList.add("is-active");
   portfolio.classList.remove("is-active");
 }
+const toggleMenu = () => {
+  if (burger.classList.contains("is-active")) {
+    burger.classList.remove("is-active");
+    navBar.classList.remove("is-active");
+  } else {
+    burger.classList.add("is-active");
+    navBar.classList.add("is-active");
+  }
+};
