644f5893ec47325f4bea521750c3bf5290198ed6
Adnan <adnan_ghafoor94@hotmail.com>
2020-08-02 06:42:42 +0100

    Made card, now need to populate it

--- src/routes/html-routes.js
+++ src/routes/html-routes.js
@@ -1,20 +1,39 @@
 const express = require("express");
+const axios = require("axios");
+
 const router = express.Router();
 
 ("https://api.github.com/users/AGhafoor94/repos");
-const baseUrl = "https://api.github.com/users/";
+const baseUrl = "https://api.github.com/users/AGhafoor94/repos";
 
 const sendHome = (req, res) => {
   res.redirect("/home");
 };
+
 const homeRoute = (req, res) => {
   res.render("home");
 };
+
 const portfolioPage = async (req, res) => {
-  res.render("projects");
+  const response = await axios({
+    url: baseUrl,
+    method: "GET",
+  });
+  res.render("projects", { response: response.data });
+};
+
+const searchRepo = async ({ body }, res) => {
+  const { repository } = body;
+  console.log(repository);
+  const response = await axios({
+    url: baseUrl,
+    method: "GET",
+  });
 };
 
 router.get("/", sendHome);
 router.get("/home", homeRoute);
 router.get("/portfolio", portfolioPage);
+router.post("/search", searchRepo);
+
 module.exports = router;
